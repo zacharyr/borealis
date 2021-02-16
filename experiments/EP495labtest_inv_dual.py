66e6c7da8e485e42bebffa4dff27c25ccd7c6a1c
radar <radar@invborealis.usask.ca>
2021-02-16 20:22:38 +0000

    Adding dual_freq split experiments from ep495

---
+++ experiments/EP495labtest_inv_dual.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+# write an experiment that creates a new control program.
+import os
+import sys
+import copy
+
+BOREALISPATH = os.environ['BOREALISPATH']
+sys.path.append(BOREALISPATH)
+
+from experiment_prototype.experiment_prototype import ExperimentPrototype
+import experiments.superdarn_common_fields as scf
+
+class EP495labtest_inv_dual(ExperimentPrototype):
+
+    def __init__(self):
+        cpid = 1051101182
+
+        if scf.IS_FORWARD_RADAR:
+            beams_to_use = scf.STD_16_FORWARD_BEAM_ORDER
+        else:
+            beams_to_use = scf.STD_16_REVERSE_BEAM_ORDER
+
+        if scf.opts.site_id in ["cly", "rkn", "inv"]:
+            num_ranges = scf.POLARDARN_NUM_RANGES
+        if scf.opts.site_id in ["sas", "pgr"]:
+            num_ranges = scf.STD_NUM_RANGES
+
+        slice_1 = {  # slice_id = 0, the first slice
+            "pulse_sequence": scf.SEQUENCE_7P,
+            "tau_spacing": scf.TAU_SPACING_7P,
+            "pulse_len": scf.PULSE_LEN_45KM,
+            "num_ranges": num_ranges,
+            "first_range": scf.STD_FIRST_RANGE,
+            "intt": 3500,  # duration of an integration, in ms
+            "beam_angle": scf.STD_16_BEAM_ANGLE,
+            "beam_order": beams_to_use,
+            "txfreq" : 10800,
+            "acf": True,
+            "xcf": True,  # cross-correlation processing
+            "acfint": True,  # interferometer acfs
+        }
+
+        slice_2 = copy.deepcopy(slice_1)
+        slice_2['txfreq'] = 13200
+
+        list_of_slices = [slice_1, slice_2]
+        sum_of_freq = 0
+        for slice in list_of_slices:
+            sum_of_freq += slice['txfreq']# kHz, oscillator mixer frequency on the USRP for TX
+        rxctrfreq = txctrfreq = int(sum_of_freq/len(list_of_slices))
+
+
+        super(EP495labtest_inv_dual, self).__init__(cpid, txctrfreq=txctrfreq, rxctrfreq=rxctrfreq,
+                comment_string='Twofsound simultaneous in-sequence')
+
+        self.add_slice(slice_1)
+
+        self.add_slice(slice_2, interfacing_dict={0: 'PULSE'})
+
