3dd60cf381a2ed716bcff4f22fa74aa08bd9a7f3
radar <radar@invborealis.usask.ca>
2021-02-16 20:31:22 +0000

    Adding ep495 experiments and code mod for tri frequency power split

--- experiment_prototype/scan_classes/sequences.py
+++ experiment_prototype/scan_classes/sequences.py
@@ -382,6 +382,11 @@ class Sequence(ScanClassBase):
         sequence = np.zeros([main_antenna_count, buffer_len], dtype=np.complex64)
 
         for slice_id in self.slice_ids:
+            #For the tri-frequency case:
+            self.basic_slice_pulses[0] *= max_usrp_dac_amplitude * 0.5
+            self.basic_slice_pulses[1] *= max_usrp_dac_amplitude * 0.3
+            self.basic_slice_pulses[2] *= max_usrp_dac_amplitude * 0.2
+
             exp_slice = self.slice_dict[slice_id]
             basic_samples = self.basic_slice_pulses[slice_id][beam_iter]  # num_antennas x num_samps
 
