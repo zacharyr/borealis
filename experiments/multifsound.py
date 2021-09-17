3a00021fbffce7ede7540c2b377365d79206a25b
radar <rar129@usask.ca>
2021-09-17 19:36:46 +0000

    Created experiment multifsound.
    * Uses 6 frequencies - 10.1, 10.6, 10.9, 11.5, 12.3, and 13.2 MHz
    * Inttime interfaced

---
+++ experiments/multifsound.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+# write an experiment that creates a new control program.
+import os
+import sys
+import copy
+
+BOREALISPATH = os.environ['BOREALISPATH']
+sys.path.append(BOREALISPATH)
+
+from experiment_prototype.experiment_prototype import ExperimentPrototype
+import experiments.superdarn_common_fields as scf
+
+class Multifsound(ExperimentPrototype):
+
+    def __init__(self):
+        cpid = 350310
+
+        if scf.IS_FORWARD_RADAR:
+            beams_to_use = scf.STD_16_FORWARD_BEAM_ORDER
+        else:
+            beams_to_use = scf.STD_16_REVERSE_BEAM_ORDER
+
+        if scf.opts.site_id in ["sas", "pgr"]:
+            freqs = (10500, 13000)
+        if scf.opts.site_id in ["rkn"]:
+            freqs = (10100, 10500, 10900, 11500, 12300, 13200) 
+        if scf.opts.site_id in ["inv"]:
+            freqs = (10300, 12200)
+        if scf.opts.site_id in ["cly"]:
+            freqs = (10500, 12500)
+
+        if scf.opts.site_id in ["cly", "rkn", "inv"]:
+            num_ranges = scf.POLARDARN_NUM_RANGES
+        if scf.opts.site_id in ["sas", "pgr"]:
+            num_ranges = scf.STD_NUM_RANGES
+
+        slice_1 = {  # slice_id = 0, the first slice
+            "pulse_sequence": scf.SEQUENCE_7P,
+            "tau_spacing": scf.TAU_SPACING_7P,
+            "pulse_len": scf.PULSE_LEN_45KM,
+            "num_ranges": num_ranges,
+            "first_range": scf.STD_FIRST_RANGE,
+            "intt": 3500,  # duration of an integration, in ms
+            "beam_angle": scf.STD_16_BEAM_ANGLE,
+            "beam_order": beams_to_use,
+            "txfreq" : freqs[0], #kHz
+            "acf": True,
+            "xcf": True,  # cross-correlation processing
+            "acfint": True,  # interferometer acfs
+        }
+
+        slice_2 = copy.deepcopy(slice_1)
+        slice_3 = copy.deepcopy(slice_1)
+        slice_4 = copy.deepcopy(slice_1)
+        slice_5 = copy.deepcopy(slice_1)
+        slice_6 = copy.deepcopy(slice_1)
+
+        slice_2['txfreq'] = freqs[1]
+        slice_3['txfreq'] = freqs[2]
+        slice_4['txfreq'] = freqs[3]
+        slice_5['txfreq'] = freqs[4]
+        slice_6['txfreq'] = freqs[5]
+
+        list_of_slices = [slice_1, slice_2, slice_3, slice_4, slice_5, slice_6]
+        sum_of_freq = 0
+        for slice in list_of_slices:
+            sum_of_freq += slice['txfreq']# kHz, oscillator mixer frequency on the USRP for TX
+        rxctrfreq = txctrfreq = 11650
+
+
+        super(Multifsound, self).__init__(cpid, txctrfreq=txctrfreq, rxctrfreq=rxctrfreq,
+                comment_string='Multifsound inttime interfaced')
+
+        self.add_slice(slice_1)
+        self.add_slice(slice_2, interfacing_dict={0: 'INTTIME'})
+        self.add_slice(slice_3, interfacing_dict={0: 'INTTIME'})
+        self.add_slice(slice_4, interfacing_dict={0: 'INTTIME'})
+        self.add_slice(slice_5, interfacing_dict={0: 'INTTIME'})
+        self.add_slice(slice_6, interfacing_dict={0: 'INTTIME'})
+
